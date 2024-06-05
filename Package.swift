c8a4466d7267e8f130aa688560505f682ac39ca5
Darkzwer <52218993+Darkzwer@users.noreply.github.com>
2024-06-05 15:16:45 +0300

    Add files via upload

---
+++ Package.swift
@@ -0,0 +1,14 @@
+// swift-tools-version:5.3
+import PackageDescription
+
+let package = Package(
+    name: "Barcode",
+    products: [
+        .library(
+            name: "Barcode",
+            targets: ["Barcode"]),
+    ],
+    targets: [
+        .binaryTarget(name: "Barcode", url: "https://pods.regulaforensics.com/Barcode/7.2.9105/DocumentReaderCore_barcode_7.2.9105.zip", checksum: "79bf65a744fd5e34fcf346737ed8e7036b7dde42d236dbb8055ee2ab9d1c9f3e"),
+    ]
+)
